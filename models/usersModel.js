670835cabb28bc2339aa6522ef4abd4f54be7353
Hemin Maru <heminmaru@Hemins-MacBook-Pro.local>
2021-06-10 02:34:52 +0530

    updates for deployment on heroku

--- models/usersModel.js
+++ models/usersModel.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const passportLocalMongoose = require("passport-local-mongoose");
 const FindorCreate = require("mongoose-findorcreate");
 
-mongoose.connect('mongodb://localhost:27017/OnlineStoreDB', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb+srv://developer-Hemin:' + process.env.DB_PASS + '@the-shop-sisters-db.2tgck.mongodb.net/the-shop-sisters-db', { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.set("useCreateIndex", true);
 
 const UserSchema = new mongoose.Schema({
