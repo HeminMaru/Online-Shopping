404660c550db3a9566b6cd782fa0aeae70a43420
Hemin Maru <heminmaru@Hemins-MacBook-Pro.local>
2021-06-21 23:29:02 +0530

    made responsive

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ initializePassport(passport);
 
 const app = express();
 
-app.use(express.static("public"));
+app.use(express.static(__dirname + '/public'));
 app.set("view-engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 
